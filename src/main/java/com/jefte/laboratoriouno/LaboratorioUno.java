0ab0b9aeff6d23e18c9bbcc0c800eaae3837268d
jeftedariel <jeftedariel3@gmail.com>
2024-09-27 22:08:37 -0600

    init

---
+++ src/main/java/com/jefte/laboratoriouno/LaboratorioUno.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.jefte.laboratoriouno;
+
+/**
+ *
+ * @author jefte
+ */
+public class LaboratorioUno {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
